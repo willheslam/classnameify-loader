6110e0dccf905f9aeadf20eeb783e8f94050d772
William Heslam <william.heslam@futurenet.com>
2015-11-18 17:52:11 +0000

    oops!

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const classnameify = require('classnameify');
 
 module.exports = function(source, map) {
 	if(this.cacheable) this.cacheable();
-	return classnameify((typeof obj === 'string') ? this.exec(source) : source);
+	return classnameify((typeof source === 'string') ? this.exec(source) : source);
 };
